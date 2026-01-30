295e1a14d849662f608f936420af7f2ac0a9274c
henselmann <henselmann@RSTVEOLIA-180>
2026-01-30 10:02:51 +0100

    Projektdateien hinzufügen.

---
+++ Inventarverwaltung/Berechtigungen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventarverwaltung
+{
+      public enum Berechtigungen
+       {
+           User = 1,
+           Admin = 2,
+      } 
+}
